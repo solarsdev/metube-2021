acc6fa2565cf770ce2aa6ac4d6ddacd836132b9e
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-07-26 15:38:35 +0900

    feat: Add morgan to log access server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const PORT = 4000;
 
 const app = express();
 
-const handleListening = () => console.log(`Server listening on port ${PORT} 🚀`);
+app.use(morgan('dev'));
+
+app.get('/', (req, res, next) => res.send('Hello, world!'));
+
+const handleListening = () =>
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
