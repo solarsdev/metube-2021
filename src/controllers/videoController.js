75c72bd2637f225d7a7f09cd87d341139405aff2
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-07-28 08:16:58 +0900

    feat: Add upload with fake db (json)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,3 +45,19 @@ export const postEdit = (req, res) => {
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
+
+export const getUpload = (req, res) => res.render('upload', { pageTitle: 'Upload Video' });
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: 'just now',
+    views: 0,
+    id: videos.length,
+  };
+  videos.push(newVideo);
+  return res.redirect('/');
+};
