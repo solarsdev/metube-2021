df2be21507dca8c2f7501bbca9b52eb2ead35d2d
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-07-27 21:42:56 +0900

    feat: Edit video with fake db (json object)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,8 +12,8 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: '2 minutes ago',
-    views: 59,
-    id: 1,
+    views: 1,
+    id: 2,
   },
   {
     title: 'Third Video',
@@ -21,13 +21,27 @@ let videos = [
     comments: 2,
     createdAt: '2 minutes ago',
     views: 59,
-    id: 1,
+    id: 3,
   },
 ];
 
 export const home = (req, res) => res.render('home', { pageTitle: 'Home', videos });
-export const search = (req, res) => res.send('search');
-export const watch = (req, res) => res.send('watch');
-export const edit = (req, res) => res.send('edit');
-export const deleteVideo = (req, res) => res.send('delete video');
-export const upload = (req, res) => res.send('upload video');
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('watch', { pageTitle: `Watching ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render('editVideo', { pageTitle: `Editing ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 
 app.use(morgan('dev'));
+app.use(express.urlencoded());
 app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
