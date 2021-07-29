3c554105d987a6ec3ab5862442f2921956c0a9ad
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-07-29 15:18:18 +0900

    feat: Connect to AWS DocumentDB

--- src/db.js
+++ src/db.js
@@ -1,12 +1,18 @@
+import fs from 'fs';
 import mongoose from 'mongoose';
 
 require('dotenv').config();
 
-const uri = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/metube-2021`;
+const uri = `mongodb://${process.env.MONGO_USER}:${encodeURIComponent(
+  process.env.MONGO_PASSWORD,
+)}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/metube-2021`;
 const options = {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  ssl: true,
+  sslValidate: false, // for dev purpose
+  sslCA: fs.readFileSync(`${__dirname}/auth/rds-combined-ca-bundle.pem`),
 };
 
 mongoose.connect(uri, options);
