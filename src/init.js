58805aeeeff618eb0a05a6d33ad2e82e6b6faf2a
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-07-29 15:53:15 +0900

    feat: Add video model and refactor server structure

---
+++ src/init.js
@@ -0,0 +1,20 @@
+import './db';
+import './models/Video';
+import app from './server';
+
+const PORT = 4000;
+const HOST = '0.0.0.0';
+
+const handleListening = () => console.log(`✅ Server listening on port http://${HOST}:${PORT} 🚀`);
+
+const server = app.listen(PORT, HOST, handleListening);
+
+process.on('SIGTERM', () => {
+  // SigTerm recieved (ECS specific)
+  // save if running job and using memory
+  // call aws sdk StopTask (gracefully shutdown)
+  // ToDo: save running jobs
+  server.close(() => {
+    debug('Nodejs backend closed');
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
-import './db';
 import express from 'express';
 import morgan from 'morgan';
 
 import globalRouter from './routers/globalRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
-
-const PORT = 4000;
-const HOST = '0.0.0.0';
 
 const app = express();
 
@@ -20,16 +16,4 @@ app.use('/', globalRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
 
-const handleListening = () => console.log(`✅ Server listening on port http://${HOST}:${PORT} 🚀`);
-
-const server = app.listen(PORT, HOST, handleListening);
-
-process.on('SIGTERM', () => {
-  // SigTerm recieved (ECS specific)
-  // save if running job and using memory
-  // call aws sdk StopTask (gracefully shutdown)
-  // ToDo: save running jobs
-  server.close(() => {
-    debug('Nodejs backend closed');
-  });
-});
+export default app;
