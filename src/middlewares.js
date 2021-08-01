f7940108aa97ac98cbfd215f01135833b53c82ec
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-08-01 23:51:21 +0900

    fix: Do not authorize if not have JWT cookie

--- src/middlewares.js
+++ src/middlewares.js
@@ -2,11 +2,17 @@ import passport from 'passport';
 
 export const localMiddleware = (req, res, next) => {
   res.locals.siteName = 'MeTube';
+
+  const jwt = req.signedCookies.jwt;
+  if (!jwt) {
+    return next();
+  }
+
   passport.authenticate('jwt', { session: false }, (error, user, info) => {
     if (user) {
       res.locals.loggedIn = true;
       res.locals.user = user;
     }
-    next();
+    return next();
   })(req, res);
 };
