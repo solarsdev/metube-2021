1d1afe13b1c3d1d69b11e2ecce9f47680d01ec6c
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-08-04 12:14:35 +0900

    feat: Add webpack and frontend development environment

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import csurf from 'csurf';
 import express from 'express';
 import morgan from 'morgan';
 import passport from 'passport';
+import path from 'path';
 import './passport';
 import { csrfMiddleware, localMiddleware, setHeaderMiddleware } from './middlewares';
 
@@ -13,18 +14,19 @@ import videoRouter from './routers/videoRouter';
 const app = express();
 const csrf = csurf({ cookie: true });
 
-app.set('views', __dirname + '/views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'pug');
 app.disable('etag');
 
 app.use(morgan('dev'));
+app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(csrf);
 app.use(csrfMiddleware);
 app.use(setHeaderMiddleware);
 app.use(localMiddleware);
-app.use(passport.initialize());
+app.use('/static', express.static(`${__dirname}/assets`));
 app.use('/', rootRouter);
 app.use('/users', userRouter);
 app.use('/videos', videoRouter);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/client/js/main.js',
+  mode: 'development',
+  watch: true,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/styles.css',
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'src', 'assets'),
+    filename: 'js/main.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
