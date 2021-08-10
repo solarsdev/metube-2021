47508b691d1a8688a198f7d622a0758de2bead6d
Sangkook Lee <sangkook.lee@solarsdev.com>
2021-08-10 17:00:09 +0900

    feat: Common form refactoring and Add upload css

--- src/client/js/videoUploader.js
+++ src/client/js/videoUploader.js
@@ -5,8 +5,8 @@ const videoUploadFilesize = document.getElementById('video-upload__filesize');
 video.addEventListener('change', (event) => {
   let { name, size } = event.target.files[0];
   const [filename, ext] = name.split('.');
-  if (filename.length > 20) {
-    name = `${filename.substr(0, 15)}...${filename.substr(-5)}.${ext}`;
+  if (filename.length > 15) {
+    name = `${filename.substr(0, 9)}...${filename.substr(-6)}.${ext}`;
   }
   videoUploadFilename.innerText = name;
   videoUploadFilesize.innerText = (size / 1024 / 1024).toFixed(1);
